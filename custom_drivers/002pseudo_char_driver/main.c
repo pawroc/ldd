417304b3c9343898cefeb31f461141edf38ce836
Pawel Drozdz <epawdroz@amazon.pl>
2022-03-25 15:19:41 +0100

    WIP Added device file creation

--- custom_drivers/002pseudo_char_driver/main.c
+++ custom_drivers/002pseudo_char_driver/main.c
@@ -1,6 +1,8 @@
 #include <linux/module.h>
 #include <linux/fs.h> // for alloc_chrdev_region
 #include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/kdev_t.h>
 
 #define DEV_MEM_SIZE 512
 
@@ -9,23 +11,68 @@ char device_buffer[DEV_MEM_SIZE];
 
 dev_t device_number;
 
-/* File operations for the driver */
-struct file_operations pcd_fops;
-
 struct cdev pcd_cdev;
+
+loff_t pcd_lseek(struct file *filp, loff_t offset, int whence)
+{
+    return 0;
+}
+
+ssize_t pcd_read(struct file *filp, char __user *buff, size_t count, loff_t * pos)
+{
+    return 0;
+}
+
+ssize_t pcd_write(struct file *filp, const char __user *buff, size_t count, loff_t *pos)
+{
+    return 0;
+}
+
+int pcd_open(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+int pcd_release(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+/* File operations for the driver */
+struct file_operations pcd_fops = {
+    .open = pcd_open,
+    .read = pcd_read,
+    .write = pcd_write,
+    .llseek = pcd_lseek,
+    .release = pcd_release,
+    .owner = THIS_MODULE
+};
+
+struct class *class_pcd;
+
+struct device *device_pcd;
 
 static int __init pcd_driver_init(void)
 {
     /*1. Dynamically allocate a device number */
-    alloc_chrdev_region(&device_number, 0, 1, "pcd");
+    alloc_chrdev_region(&device_number, 0, 1, "pcd_devices");
+
+    pr_info("%s : Device number <major>.<minor> = %d.%d\n", __func__, MAJOR(device_number), MINOR(device_number));
 
     /* 2. Initialize a cdev structure */
-    // pcf_fops.owner = THIS_MODULE; // ????
     cdev_init(&pcd_cdev, &pcd_fops);
 
     /* 3. Register a device (cdev structure) with VFS */
     pcd_cdev.owner = THIS_MODULE;
     cdev_add(&pcd_cdev, device_number, 1);
+
+    /*4. Create device class under /sys/class */
+    class_pcd = class_create(THIS_MODULE, "ocd_class");
+
+    /*5. Populate the sysfs with device information */
+    device_pcd = device_create(class_pcd, NULL, device_number, NULL, "pcd");
+
+    pr_info("Module init was succesfull\n");
 
     return 0;
 }
